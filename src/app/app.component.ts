dc9109277a68182c46f5a011165d7fbfa2635352
aisharay <74696016+aisharay@users.noreply.github.com>
2022-01-18 22:35:02 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import {WeatherService} from './weather.service'
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor( private WeatherData:WeatherService){ }
+  weatherData={
+    main:{temp : 0,humidity:0,temp_max:0,temp_min:0,feels_like:0},
+    sys:{ sunset:0},
+    isDay:true,
+    temp_celcius:"",
+    temp_max:"",
+    temp_min:"",
+    temp_feels_like:"",
+    name: "",
+    sunset_time:""
+   
+    
+  }
+  error=null;
+  ngOnInit(){
+         this.WeatherData.getWeatherData().subscribe((data: any)=>{
+         console.warn("data",data)
+         this.weatherData = data; 
+         let sunsetTime = new Date(this.weatherData.sys.sunset * 1000); 
+         this.weatherData.sunset_time = sunsetTime.toLocaleTimeString(); 
+         let currentDate = new Date(); 
+         this.weatherData.isDay = (currentDate.getTime() < sunsetTime.getTime()); 
+         this.weatherData.temp_celcius = (this.weatherData.main.temp - 273.15).toFixed(0); 
+         this.weatherData.temp_min = (this.weatherData.main.temp_min - 273.15).toFixed(0); 
+         this.weatherData.temp_max = (this.weatherData.main.temp_max - 273.15).toFixed(0); 
+         this.weatherData.temp_feels_like = (this.weatherData.main.feels_like - 273.15).toFixed(0);  
+  },
+  (error)=>{
+    console.log(error);
+this.error =error.name;
+  }
+  )
+  
+}}
---
+++ src/app/weather.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+     url="https://api.openweathermap.org/data/2.5/weather?q=bhubaneswar&appid=ff1bc4683fc7325e9c57e586c20cc03e"
+  
+    
+     constructor(private http:HttpClient) { }
+  
+    getWeatherData(){
+       return this.http.get(this.url);
+    }
+    
+}
